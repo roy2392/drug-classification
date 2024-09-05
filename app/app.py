52c54c3d60b1b39263f05718e1b2ec34c3de7fa5
Restyled.io <commits@restyled.io>
2024-09-05 04:25:58 +0000

    Restyled by black

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import gradio as gr
 import skops.io as sio
+
 unknown_types = sio.get_untrusted_types(file="./Model/drug_pipeline.skops")
 pipe = sio.load("./Model/drug_pipeline.skops", trusted=unknown_types)
 
