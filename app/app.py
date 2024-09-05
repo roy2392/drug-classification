994d771a63279dff3f6d4a33a045a1859797244c
Restyled.io <commits@restyled.io>
2024-09-05 04:26:08 +0000

    Restyled by yapf

--- app/app.py
+++ app/app.py
@@ -44,11 +44,9 @@ examples = [
     [50, "M", "HIGH", "HIGH", 34],
 ]
 
-
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
 article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
-
 
 gr.Interface(
     fn=predict_drug,
