e05cdf6713668983eecf45494b99d45aca9b3736
Restyled.io <commits@restyled.io>
2024-09-05 04:26:03 +0000

    Restyled by pyment

--- app/app.py
+++ app/app.py
@@ -8,15 +8,19 @@ pipe = sio.load("./Model/drug_pipeline.skops", trusted=unknown_types)
 def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
     """Predict drugs based on patient features.
 
-    Args:
-        age (int): Age of patient
-        sex (str): Sex of patient
-        blood_pressure (str): Blood pressure level
-        cholesterol (str): Cholesterol level
-        na_to_k_ratio (float): Ratio of sodium to potassium in blood
+    :param age: Age of patient
+    :type age: int
+    :param sex: Sex of patient
+    :type sex: str
+    :param blood_pressure: Blood pressure level
+    :type blood_pressure: str
+    :param cholesterol: Cholesterol level
+    :type cholesterol: str
+    :param na_to_k_ratio: Ratio of sodium to potassium in blood
+    :type na_to_k_ratio: float
+    :returns: Predicted drug label
+    :rtype: str
 
-    Returns:
-        str: Predicted drug label
     """
     features = [age, sex, blood_pressure, cholesterol, na_to_k_ratio]
     predicted_drug = pipe.predict([features])[0]
